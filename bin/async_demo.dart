f9356ff9ba37945bd23ec129621b87ca2e7363f3
Ellen Barros <120993832+Nellefb@users.noreply.github.com>
2024-10-13 19:08:02 -0300

    Update async_demo.dart

--- bin/async_demo.dart
+++ bin/async_demo.dart
@@ -3,48 +3,68 @@
 
 // 1 execute o método de login informando o e-mail e a senha
 // 2 caso o email e senha sejam válidos, consulte o saldo do usuário informando o token e printando o resultado no console
-// 3 caso o email ou senha sejam inválidos, printe a mensagem de erro. 
+// 3 caso o email ou senha sejam inválidos, printe a mensagem de erro.
 
+//criar proj dart dart create (nome)
+//.then é quando concluido com sucesso
 
 import 'dart:ffi';
 
-void main(){
+void main() {
   consulta();
   print("Fim");
 }
 
 //para pegar o token direto, deve clocar await e async
-void consulta() async{
-  try{
+void consulta() async {
+  try {
     String token = await login('email@email.com', '123');
-  num saldo = await consultarSaldo(token);
-  print(saldo);
-  } catch(error){
+    num saldo = await consultarSaldo(token);
+    print(saldo);
+  } catch (error) {
     print(error);
   }
 }
 
+Future<String> login(String email, String senha) async {
+  // Simulando um atraso de download de 2 segundos
+  await Future.delayed(Duration(seconds: 2));
+
+  //se login é valido
+  if (email != 'email@email.com' || senha != '123') {
+    throw Exception('email ou senha inválida');
+  }
+
+  //retorna o token se o email e senha corretos
+  // Simulando informações de token de acesso do usuário
+  final token = 'bas89e8ahdkkdassd';
+
+  return token;
+}
+
+//recebe o token se login for valido, retorna num
+Future<num> consultarSaldo(String token) async {
+  // Simulando um atraso de download de 3 segundos
+  await Future.delayed(Duration(seconds: 3));
+
+  return 2000;
+}
 
 void main2() {
   Future<String> futureToken = login('email@email.com', '123');
-  futureToken.then((String token){
+  futureToken.then((String token) {
     //Concluido com sucesso
     //consultarSaldo(token);
     //não da pa printar o consultar saldo pois ele retrna future, precisa do then no value
-    consultarSaldo(token).then((num saldo){
+    consultarSaldo(token).then((num saldo) {
       print(saldo);
     });
-  }).catchError((error){
+  }).catchError((error) {
     //Concluido com erro
     print(error);
   });
 
 //main executa antes da future, depois ele printa o dado da future
-
-
-
-
-
   //Future<void> future = login('email@email.com', '123').then((token){
   // Future<void> future = login('email@email', '123').then((token){
   //   consultarSaldo(token).then((saldo) {
@@ -53,30 +73,6 @@ void main2() {
   // }).catchError((value){
   //   print(value);
   // });
-  
 }
-
-
-Future<String> login(String email, String senha) async {
-  // Simulando um atraso de download de 2 segundos
-  await Future.delayed(Duration(seconds: 2));
-  
-  if(email != 'email@email.com' || senha != '123') {
-    throw Exception('email ou senha inválida');
-  }
-
-  // Simulando informações de token de acesso do usuário
-  final token = 'bas89e8ahdkkdassd';
-
-  return token;
-}
-
-Future<num> consultarSaldo(String token) async {
-     // Simulando um atraso de download de 3 segundos
-  await Future.delayed(Duration(seconds: 3));
-  
-  return 2000;
-}
-
 
 //https://www.canva.com/design/DAFrFIrk-Lc/n_v7URQguV59lPi1T-Vbhg/edit
